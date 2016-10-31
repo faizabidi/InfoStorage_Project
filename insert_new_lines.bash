6e7300cb4b63bd419533a84f248d2cf85c237731
Faiz <fabidi89@vt.edu>
2016-10-31 01:33:21 -0400

    Minor changes.

--- insert_new_lines.bash
+++ insert_new_lines.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Insert newlines after the keyword "twitter-serach"
-CMD1="sed -r 's/twitter\-search/\\ntwitter\-search/g' remove_newlines.txt > insert_newlines.txt"
+CMD1="sed -r 's/twitter\-search/\\ntwitter\-search/g' newlines_removed.txt > cleaned1.txt"
 eval $CMD1
 
 # Insert newlines after the keyword "twitter-stream"
-CMD2="sed -r 's/twitter\-stream/\\ntwitter\-stream/g' insert_newlines.txt > insert_newlines.txt"
+CMD2="sed -r 's/twitter\-stream/\\ntwitter\-stream/g' cleaned1.txt > cleaned2.txt"
 eval $CMD2
--- remove_all_newlines.bash
+++ remove_all_newlines.bash
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 filename=$1
-echo $filename
 
 # Remove all newlines
-tr -d '\n' < $filename > remove_newlines.txt
-
-# Insert newlines after the keyword "twitter-serach"
-# sed 's/twitter-search/&\n/g' remove_newlines.txt > insert_newlines.txt
+tr -d '\n' < $filename > newlines_removed.txt
