ca5f4ba4764656271552c7bc653402d23f2eafe0
Faiz <fabidi89@vt.edu>
2016-11-25 17:16:09 -0500

    Convert csv to avro.

--- convert_csv_avro.bash
+++ convert_csv_avro.bash
@@ -1,7 +1,23 @@
 #!/bin/bash
-csv2avro \
-	--schema schema.avsc \
+
+if [ $# -ne 2 ]; then
+	echo "Provide exactly two arguments - path to the file, and path to the schema."
+	exit 1
+fi
+
+FILE_PATH=$1
+SCHEMA_PATH=$2
+
+FILE=$(basename $FILE_PATH)
+SCHEMA=($basename $SCHEMA_PATH)
+
+if csv2avro \
+	--schema $SCHEMA_PATH \
 	--delimiter "\t" \
 	--line-ending "\n" \
-	--bad-rows ~/avro_data/rows3.bad \
-	~/avro_data/2016-11-22-test_faiz.z_312.csv
+	--bad-rows bad.rows \
+	$FILE_PATH; then
+	echo "Conversion to avro successful!"
+else
+	echo "Conversion to avro not successful."
+fi
