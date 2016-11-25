37340d9315de9aeb43bee543709844a2b93c3e2e
Faiz <fabidi89@vt.edu>
2016-11-25 04:00:21 -0500

    Remove deleted files.

--- convert_csv_avro.bash
+++ convert_csv_avro.bash
@@ -3,6 +3,5 @@ csv2avro \
 	--schema schema.avsc \
 	--delimiter "\t" \
 	--line-ending "\n" \
-	--bad-rows rows.bad \
-	--write-defaults
-	~/2016-11-22-test_faiz.z_312
+	--bad-rows ~/avro_data/rows3.bad \
+	~/avro_data/2016-11-22-test_faiz.z_312.csv
