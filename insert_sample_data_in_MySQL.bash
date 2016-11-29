d1441057e1a3675484801c36a2a774b7fdf6b2b2
Faiz Abidi <fabidi89@vt.edu>
2016-11-29 15:49:35 -0500

    Added failure/success message.

--- insert_sample_data_in_MySQL.bash
+++ insert_sample_data_in_MySQL.bash
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-mysql -u root -p'infostorage' test_faiz < ~/sample_msql_data/Dump20160919.sql
+# Transfer the AQL data into the MySQL database
+if mysql -u root -p'infostorage' test_faiz < ~/sample_msql_data/Dump20160919.sql; then
+    echo Import of data into MySQL successful!
+else
+    echo Import of data into MySQL unsuccessful.
+fi
 
