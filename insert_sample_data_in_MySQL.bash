7a2a593815f92d23b65c4ca6aaf0ec350efa8da8
Faiz <fabidi89@vt.edu>
2016-12-01 14:27:12 -0500

    Added an argument to take filename.

--- insert_sample_data_in_MySQL.bash
+++ insert_sample_data_in_MySQL.bash
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+MYSQLFILE=$1
+
+if [ $# -ne 1 ]; then
+	echo "Enter the SQL filename."
+	exit
+fi
+
 # Transfer the AQL data into the MySQL database
-if mysql -u root -p'infostorage' test_faiz < ~/sample_msql_data/Dump20160919.sql; then
+if mysql -u root -p'infostorage' infostorage < $MYSQLFILE; then
     echo Import of data into MySQL successful!
 else
     echo Import of data into MySQL unsuccessful.
