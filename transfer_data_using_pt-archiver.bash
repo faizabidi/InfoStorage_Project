69437e885e30dae8271cfc661a08e48755b2b479
Faiz <fabidi89@vt.edu>
2016-12-01 15:05:21 -0500

    Added command line arguments.

--- transfer_data_using_pt-archiver.bash
+++ transfer_data_using_pt-archiver.bash
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+HOST=$1
+DATABASE=$2
+TABLENAME=$3
+
+if [ $# -ne 3 ]; then
+	echo "Provide host name, database name, and table name only."
+	echo "For example, localhost infostorage z_703"
+	echo "We assume you have a table called "Archives" in your database."
+	exit
+fi
+
 # Run pt-archiver to transfer tweets to the Archives table
 # Also output contents into a text file
 # Note that pt-archiver MOVES tweets to the Archives table.
-# That means that tweets will be deleted from the original table 
-cmd="sudo pt-archiver --source h=localhost,D=test_faiz,t=z_312 \
+# That means that tweets will be deleted from the original table
+cmd="sudo pt-archiver --source h=$HOST,D=$DATABASE,t=$TABLENAME \
     -u root -p'infostorage' \
-    --dest h=localhost,D=test_faiz,t=Archives \
-    --file '/home/ubuntu/sample_msql_data/%Y-%m-%d-%D.%t' \
+    --dest h=$HOST,D=$DATABASE,t=Archives \
+    --file '%Y-%m-%d-%D.%t' \
     --where "1=1" --statistics --ignore"
 eval $cmd
 
